8cf5e2991f28e5db3868087d133a584800d4c7f1
kemar-art <58619137+kemar-art@users.noreply.github.com>
2023-09-18 16:19:21 -0500

    first commit

---
+++ ifstatement.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "<h2>If Statement</h2>";
+
+        $grade = 50;
+
+        if ($grade >= 50) {
+            echo "<h3>You Have Pass</3>";
+        }
+        else{
+            echo "<h3>You Have Failed</3>";
+        }
+    ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2>Follow each link to the page example</h2>
+  <ul>
+    <li><a href="array.php">Simple Array and Printouts</a></li>
+    <li><a href="forloop.php">Simple For Loop</a></li>
+    <li><a href="dowhileloop.php">Simple Do While</a></li>
+    <li><a href="whileloop.php">Simple While Loop</a></li>
+    <li><a href="ifstatement.php">Simple If Statement</a></li>
+    <li><a href="switchstatement.php">Simple Switch Statement</a></li>
+  </ul>
+
+  <h1>Hello World</h1>
+
+
+  <?php 
+    echo"This is PHP";
+  ?>
+
+  <?php 
+    $name = "Kemar Norton";
+    echo"<h1>My name is: $name</h1>";
+  ?>
+</body>
+</html>
